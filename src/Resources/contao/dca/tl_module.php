08313c9f05218199faae42622fedf08838f97fd8
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2018-01-11 14:31:42 +0100

    change palette

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -10,7 +10,7 @@
  */
 
 
-$GLOBALS['TL_DCA']['tl_module']['palettes']['newslist'] = str_replace(',skipFirst', ',skipFirst,relatedOnly,disableEmpty', $GLOBALS['TL_DCA']['tl_module']['palettes']['newslist']);
+$GLOBALS['TL_DCA']['tl_module']['palettes']['newslist'] = str_replace(',perPage', ',perPage,relatedOnly,disableEmpty', $GLOBALS['TL_DCA']['tl_module']['palettes']['newslist']);
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['relatedOnly'] = array
 (
