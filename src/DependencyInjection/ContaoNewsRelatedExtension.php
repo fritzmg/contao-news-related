b5d4aefa377152d2252b322dce5aee4f47b836cd
fritzmg <fmg@inspiredminds.at>
2023-04-06 13:53:41 +0100

    fix services

--- src/DependencyInjection/ContaoNewsRelatedExtension.php
+++ src/DependencyInjection/ContaoNewsRelatedExtension.php
@@ -22,7 +22,7 @@ class ContaoNewsRelatedExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('listener.yml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../config'));
+        $loader->load('services.yaml');
     }
 }
